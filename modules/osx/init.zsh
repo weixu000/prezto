6cfe8ace7e65ee4208fad760605e9cfe840550f4
Xu Wei <18422432+weixu000@users.noreply.github.com>
2023-12-09 15:35:21 +0800

    Export http proxy envar in osx module

--- modules/osx/init.zsh
+++ modules/osx/init.zsh
@@ -22,3 +22,28 @@ alias cdf='cd "$(pfd)"'
 
 # Pushes directory to the current Finder directory.
 alias pushdf='pushd "$(pfd)"'
+
+# Export http proxy to envar if set
+export http_proxy=$(scutil --proxy | awk '\
+  /HTTPEnable/ { enabled = $3; } \
+  /HTTPProxy/ { server = $3; } \
+  /HTTPPort/ { port = $3; } \
+  END { if (enabled == "1") { print server ":" port; } }')
+
+export https_proxy=$(scutil --proxy | awk '\
+  /HTTPSEnable/ { enabled = $3; } \
+  /HTTPSProxy/ { server = $3; } \
+  /HTTPSPort/ { port = $3; } \
+  END { if (enabled == "1") { print server ":" port; } }')
+
+if [[ -n "$http_proxy" ]]; then
+  print "http_proxy=$http_proxy"
+else
+  unset http_proxy
+fi
+
+if [[ -n "$https_proxy" ]]; then
+  print "https_proxy=$https_proxy"
+else
+  unset https_proxy
+fi
